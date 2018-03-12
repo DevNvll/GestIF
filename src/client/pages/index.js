1a995a2afa6fa720f0fa3d29dabd9a60ca806bae
Brenno Luis <bluis1707@gmail.com>
2018-03-12 15:14:43 -0300

    Pagina inicial modificada

--- src/client/pages/index.js
+++ src/client/pages/index.js
@@ -11,63 +11,20 @@ class Home extends Component {
         <Head title="GestIF" />
         <div className="content-wrapper">
           <section className="content-header">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="row">
-                  <div className="col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                      <span className="info-box-icon bg-black">
-                        <i className="fab fa-steam" />
-                      </span>
+            <h1>
+              Início <small>Início do painel de controle</small>
+            </h1>
+            <ol className="breadcrumb">
+              <li>
+                <a href="#">
+                  <i className="fa fa-dashboard" /> Início
+          </a>
+              </li>
 
-                      <div className="info-box-content">
-                        <span className="info-box-text">Games</span>
-                        <span className="info-box-number">330</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                      <span className="info-box-icon bg-red">
-                        <i className="fab fa-lastfm" />
-                      </span>
-
-                      <div className="info-box-content">
-                        <span className="info-box-text">Scrobbles</span>
-                        <span className="info-box-number">41,410</span>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="clearfix visible-sm-block" />
-
-                  <div className="col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                      <span className="info-box-icon bg-green">
-                        <i className="fa fa-shopping-cart" />
-                      </span>
-
-                      <div className="info-box-content">
-                        <span className="info-box-text">Sales</span>
-                        <span className="info-box-number">760</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                      <span className="info-box-icon bg-yellow">
-                        <i className="fa fa-id-card" />
-                      </span>
-
-                      <div className="info-box-content">
-                        <span className="info-box-text">New Members</span>
-                        <span className="info-box-number">2,000</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            </ol>
+          </section>
+          <section className="content">
+            Bem vindos ao sistema de controle de estoque!
           </section>
         </div>
       </Page>
