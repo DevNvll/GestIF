5bf1a8efc5c617cfc14cd932a379427679bb39f0
Henrick Mello <devnoir@protonmail.com>
2018-03-18 22:22:28 -0300

    adicionado criptografia para senhas

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,6 +1,7 @@
 import {Router} from 'express'
 import User from '../../models/Users'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 const router = Router()
 
@@ -11,35 +12,49 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   User.findOne({ email: req.body.email })
     .then(result => {
-      if (req.body.password === result.password) {
-        jwt.sign(
-          { id: result._id },
-          req.app.get('secret'),
-          (err, token) => {
-            res.status(200).json({
-              code: 'LOGIN_SUCCESS',
-              result: {
-                profile: {
-                  email: result.email,
-                  name: result.name,
-                  join_date: result.joined
-                },
-                access_token: token
-              }
-            })
-          }
-        )
-      } else {
-        res.json({
-          code: 'LOGIN_FAIL',
-          result: {}
-        })
-      }
+      bcrypt.compare(req.body.password, result.password).then(passed => {
+        if(passed) {
+          jwt.sign(
+            { id: result._id },
+            req.app.get('secret'),
+            (err, token) => {
+              res.status(200).json({
+                code: 'LOGIN_SUCCESS',
+                result: {
+                  profile: {
+                    email: result.email,
+                    name: result.name,
+                    join_date: result.joined
+                  },
+                  access_token: token
+                }
+              })
+            }
+          )
+        } else {
+          res.json({
+            code: 'LOGIN_FAIL',
+            result: {}
+          })
+        }
+      })
     })
     .catch(err => {
       console.log(err)
       res.send('Error creating user')
     })
+})
+
+router.post('/register', (req, res) => {
+  const {name, email, password} = req.body
+  bcrypt.hash(password, 10).then(hash => {
+    const newUser = new User({name, email, password: hash})
+    newUser.save().then(user => {
+      console.log('added', user.name)
+      res.send(user)
+    })
+  })
+  
 })
 
 export default router
