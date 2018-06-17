b7363c9b09b63db1c1b50891ececfa404124da1f
Henrick Mello <devnoir@protonmail.com>
2018-06-17 12:02:38 -0300

    Consertado erro de responsividade

--- src/client/pages/csti/index.js
+++ src/client/pages/csti/index.js
@@ -55,117 +55,121 @@ class Home extends Component {
             </ol>
           </section>
           <section className="content">
-            <div className="col-lg-3 col-xs-6">
-              <div className="small-box bg-yellow">
-                <div className="inner">
-                  <h3>{this.state.stats.numPendentes}</h3>
-                  <p>Reports Pendentes</p>
+            <div className="row">
+              <div className="col-lg-3 col-xs-6">
+                <div className="small-box bg-yellow">
+                  <div className="inner">
+                    <h3>{this.state.stats.numPendentes}</h3>
+                    <p>Reports Pendentes</p>
+                  </div>
+                  <div className="icon">
+                    <i
+                      className="fa fa-clipboard"
+                      style={{ paddingTop: '20px', fontSize: '80px' }}
+                    />
+                  </div>
+                  <Link href="/csti/reports">
+                    <a className="small-box-footer">
+                      Ir para reports <i className="fa fa-arrow-circle-right" />
+                    </a>
+                  </Link>
                 </div>
-                <div className="icon">
-                  <i
-                    className="fa fa-clipboard"
-                    style={{ paddingTop: '20px', fontSize: '80px' }}
-                  />
+              </div>
+              <div className="col-lg-3 col-xs-6">
+                <div className="small-box bg-red">
+                  <div className="inner">
+                    <h3>{this.state.stats.maisDeUmaSemana}</h3>
+                    <p>Reports pendentes por + de uma semana</p>
+                  </div>
+                  <div className="icon">
+                    <i
+                      className="fa fa-times"
+                      style={{ paddingTop: '20px', fontSize: '80px' }}
+                    />
+                  </div>
+                  <Link href="/csti/reports">
+                    <a className="small-box-footer">
+                      Ir para reports <i className="fa fa-arrow-circle-right" />
+                    </a>
+                  </Link>
                 </div>
-                <Link href="/csti/reports">
-                  <a className="small-box-footer">
-                    Ir para reports <i className="fa fa-arrow-circle-right" />
-                  </a>
-                </Link>
+              </div>
+              <div className="col-lg-3 col-xs-6">
+                <div className="small-box bg-aqua">
+                  <div className="inner">
+                    <h3>{this.state.stats.numResolvidoPor}</h3>
+                    <p>Reports resolvidos por você</p>
+                  </div>
+                  <div className="icon">
+                    <i
+                      className="fa fa-check"
+                      style={{ paddingTop: '20px', fontSize: '80px' }}
+                    />
+                  </div>
+                  <Link href="/csti/reports">
+                    <a className="small-box-footer">
+                      Ir para reports <i className="fa fa-arrow-circle-right" />
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <div className="col-lg-3 col-xs-6">
+                <div className="small-box bg-green">
+                  <div className="inner">
+                    <h3>{this.state.stats.numTotal}</h3>
+                    <p>Reports no total</p>
+                  </div>
+                  <div className="icon">
+                    <i
+                      className="fa fa-chart-bar"
+                      style={{ paddingTop: '20px', fontSize: '80px' }}
+                    />
+                  </div>
+                  <Link href="/csti/reports">
+                    <a className="small-box-footer">
+                      Ir para reports <i className="fa fa-arrow-circle-right" />
+                    </a>
+                  </Link>
+                </div>
               </div>
             </div>
-            <div className="col-lg-3 col-xs-6">
-              <div className="small-box bg-red">
-                <div className="inner">
-                  <h3>{this.state.stats.maisDeUmaSemana}</h3>
-                  <p>Reports pendentes por + de uma semana</p>
-                </div>
-                <div className="icon">
-                  <i
-                    className="fa fa-times"
-                    style={{ paddingTop: '20px', fontSize: '80px' }}
-                  />
-                </div>
-                <Link href="/csti/reports">
-                  <a className="small-box-footer">
-                    Ir para reports <i className="fa fa-arrow-circle-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-lg-3 col-xs-6">
-              <div className="small-box bg-aqua">
-                <div className="inner">
-                  <h3>{this.state.stats.numResolvidoPor}</h3>
-                  <p>Reports resolvidos por você</p>
-                </div>
-                <div className="icon">
-                  <i
-                    className="fa fa-check"
-                    style={{ paddingTop: '20px', fontSize: '80px' }}
-                  />
-                </div>
-                <Link href="/csti/reports">
-                  <a className="small-box-footer">
-                    Ir para reports <i className="fa fa-arrow-circle-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-lg-3 col-xs-6">
-              <div className="small-box bg-green">
-                <div className="inner">
-                  <h3>{this.state.stats.numTotal}</h3>
-                  <p>Reports no total</p>
-                </div>
-                <div className="icon">
-                  <i
-                    className="fa fa-chart-bar"
-                    style={{ paddingTop: '20px', fontSize: '80px' }}
-                  />
-                </div>
-                <Link href="/csti/reports">
-                  <a className="small-box-footer">
-                    Ir para reports <i className="fa fa-arrow-circle-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="box box-info">
-                <div className="box-header with-border">
-                  <h3 className="box-title">
-                    Gráfico de reports nos últimos seis meses
-                  </h3>
-                </div>
-                <div className="box-body">
-                  <div className="chart">
-                    {this.state.count && (
-                      <ChartReports
-                        data={{
-                          total: this.state.count,
-                          byMe: this.state.byMe
-                        }}
-                      />
-                    )}
+            <div className="row">
+              <div className="col-md-6">
+                <div className="box box-info">
+                  <div className="box-header with-border">
+                    <h3 className="box-title">
+                      Gráfico de reports nos últimos seis meses
+                    </h3>
+                  </div>
+                  <div className="box-body">
+                    <div className="chart">
+                      {this.state.count && (
+                        <ChartReports
+                          data={{
+                            total: this.state.count,
+                            byMe: this.state.byMe
+                          }}
+                        />
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-6">
-              <div className="box box-success">
-                <div className="box-header with-border">
-                  <h3 className="box-title">Logs do sistema</h3>
-                </div>
-                <div className="box-body">
-                  <TableLogs logs={this.state.logs} />
-                  <Link href="csti/logs">
-                    <a>
-                      <Button secondary fluid style={{ marginTop: '10px' }}>
-                        Ver todos os logs
-                      </Button>
-                    </a>
-                  </Link>
+              <div className="col-md-6">
+                <div className="box box-success">
+                  <div className="box-header with-border">
+                    <h3 className="box-title">Logs do sistema</h3>
+                  </div>
+                  <div className="box-body">
+                    <TableLogs logs={this.state.logs} />
+                    <Link href="csti/logs">
+                      <a>
+                        <Button secondary fluid style={{ marginTop: '10px' }}>
+                          Ver todos os logs
+                        </Button>
+                      </a>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
